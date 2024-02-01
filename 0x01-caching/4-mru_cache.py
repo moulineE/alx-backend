3068592fdf2c00daa807a08f3eab049d30868984
moulineE <elmahdi.mouline@live.fr>
2024-02-01 09:01:15 +0100

    Task 4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+4. MRU Caching module
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """a class that inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        super().__init__()
+        self.keys = {}
+        self.MRU_nb = 0
+
+    def put(self, key, item):
+        """
+        a class that inherits from BaseCaching and is a caching system
+        implementing a MRU algorithm
+        """
+        if key and item:
+            if key in self.keys.keys():
+                self.cache_data[key] = item
+                self.keys[key] = self.MRU_nb
+                self.MRU_nb += 1
+            else:
+                if len(self.keys) >= self.MAX_ITEMS:
+                    del_key = max(self.keys, key=self.keys.get)
+                    del self.cache_data[del_key]
+                    print("DISCARD: {}".format(del_key))
+                    del self.keys[del_key]
+                    self.cache_data[key] = item
+                    self.keys[key] = self.MRU_nb
+                    self.MRU_nb += 1
+                else:
+                    self.keys[key] = self.MRU_nb
+                    self.MRU_nb += 1
+                    self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key is None or key not in self.keys.keys():
+            return None
+        self.keys[key] = self.MRU_nb
+        self.MRU_nb += 1
+        return self.cache_data[key]
