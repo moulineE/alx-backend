780cc779e3b80ac6d43e42b9e7d410122fa3ae8b
moulineE <elmahdi.mouline@live.fr>
2024-02-06 18:11:43 +0100

    Task 3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""a basic Flask app"""
+from flask import (Flask, render_template, request)
+from flask_babel import Babel
+
+
+class Config:
+    """config for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    """render 3-index.html"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
