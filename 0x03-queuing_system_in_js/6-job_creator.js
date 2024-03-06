4e8462d5dc3a79953cfd2b9880945e5c2cc40538
moulineE <elmahdi.mouline@live.fr>
2024-03-06 08:52:18 +0100

    Task 7. Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: 'string',
-  message: 'string'
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account'
 }).save(err => {
   if (!err) console.log(`Notification job created: ${job.id}`);
 });
